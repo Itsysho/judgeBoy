10727e1b3fdbb5a2c67a4e77527d74aede297881
YS Ho <itisysho@gmail.com>
2026-09-07 16:45:39 +0800

    feat: add account models

---
+++ judgeServer/apps/accounts/models.py
@@ -0,0 +1,16 @@
+import uuid
+from django.db import models
+# https://docs.djangoproject.com/en/6.0/ref/contrib/auth/#django-contrib-auth
+from django.contrib.auth.models import AbstractUser
+
+# a table in DB
+
+
+class User(AbstractUser):
+    # Inheritance from AbstractUser
+    # a colume in table
+    # public-facing identifier, so we don't expose the sequential pk
+    # would call uuid.uuid4 everytime
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    motto = models.TextField(blank=True)
+    user_ip = models.GenericIPAddressField(null=True, blank=True)
